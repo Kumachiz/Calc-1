ff468f08287585bd84732da757214121fc9e444d
Michael Munyaneza and Stephen Emery <emery.stephen@gmail.com>
2016-05-18 20:39:06 -0700

    Add red, green, yellow buttons and javascript to add classes and css to change background colors

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,23 @@ $(function (){
     $("#initially-hidden").fadeToggle();
     $("#initially-showing").toggle();
   });
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
   $("#calculator").click(function() {
     var add = function(number1, number2) {
-      return number1 + number2;
+      return number1 + number2 + 5;
     };
 
     var number1 = parseInt(prompt("Enter a number:"));
